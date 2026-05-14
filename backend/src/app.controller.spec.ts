091ccfd0e157639149c021d060f176c7cfe4bb97
Hamdi Molka <Molkahamdi123@gmail.com>
2026-05-14 18:12:28 +0100

    fix error

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -14,8 +14,8 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "WELCOME TO ATBDIGIPACK!"', () => {
+      expect(appController.getHello()).toBe('WELCOME TO ATBDIGIPACK!');
     });
   });
 });
