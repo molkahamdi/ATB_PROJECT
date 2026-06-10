af30b2a56e1c008778e08bf2b858ec9442a622b9
Hamdi Molka <Molkahamdi123@gmail.com>
2026-06-10 12:02:00 +0100

    test2: automatic deployment

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'WELCOME TO ATBDIGIPACK!';
+    return 'WELCOME TO ATBDIGIPACKV2!';
   }
 }
